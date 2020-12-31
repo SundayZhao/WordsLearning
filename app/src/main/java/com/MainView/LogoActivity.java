bd0167d4c89cf791b45bd23deec921f2a2c956c0
SundayZhao <664834952@qq.com>
2020-12-31 10:47:51 +0800

    finish

---
+++ app/src/main/java/com/MainView/LogoActivity.java
@@ -0,0 +1,30 @@
+package com.MainView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.R;
+
+
+/**
+ * Created by gangchang on 2018/4/24.
+ */
+
+public class LogoActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasiduo);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                /* Create an Intent that will start the Main WordPress Activity. */
+                Intent mainIntent = new Intent(LogoActivity.this, MainActivity.class);
+                LogoActivity.this.startActivity(mainIntent);
+                LogoActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
